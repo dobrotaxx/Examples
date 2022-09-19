55eebbc37897bae978c95c77e273b07bc92089ed
Semyon <tonkih.sem@mail.ru>
2022-09-19 23:59:49 +0500

    add Exampole 010

---
+++ Example010_methodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {12, 18, 33, 54, 58, 26, 97, 18,};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
