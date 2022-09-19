13afb5116883d0b89a411fc4b7f07921e6200fcb
Semyon <tonkih.sem@mail.ru>
2022-09-19 16:53:34 +0500

    add Example 009

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -1,50 +1,14 @@
-﻿/*int a1 = 15;
-int b1 = 21;
-int c1 = 39;
-int a2 = 12;
-int b2 = 23;
-int c2 = 33;
-int a3 = 432;
-int b3 = 23;
-int c3 = 33;
-
-int max = a1;
-if (b1 > max) max = b1;
-if (c1 > max) max = c1;
-
-if (a2 > max) max = a2;
-if (b2 > max) max = b2;
-if (c2 > max) max = c2;
-
-if (a3 > max) max = a3;
-if (b3 > max) max = b3;
-if (c3 > max) max = c3;
-
-Console.WriteLine(max); - Стихийный метод*/
-
-int Max(int arg1, int arg2, int arg3)
+﻿int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if (arg2 > result) result = arg2;
     if (arg3 > result) result = arg3;
     return result;
 }
+//               0   1   2    3    4   5   6   7   8
+int [] array = {13, 120, 32, 406, 530, 61 ,17 ,85 ,19};
+array[0] = 12;
 
-int a1 = 151414;
-int b1 = 21;
-int c1 = 391231;
-int a2 = 12;
-int b2 = 1251;
-int c2 = 33123123;
-int a3 = 432;
-int b3 = 2341421;
-int c3 = 33;
-
-// int max1 = Max(a1, b1, c1);
-// int max2 = Max(a2, b2, c2);
-// int max3 = Max(a3, b3, c3);
-// int max = Max(max1, max2, max3);
-
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
 
 Console.WriteLine(max);
